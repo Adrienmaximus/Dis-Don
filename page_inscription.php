7b700cc70d65867f90827fc609af5751f156dd11
Adrienmaximus <adriencenik@gmail.com>
2024-01-09 20:28:31 +0100

    first commit

---
+++ page_inscription.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire Inscription
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <!-----------------------------Partie sup------------------------------->
+        <div class="col-12 d-flex justify-content-end">
+            <div class="col-7 row rounded m-3 p-1 text-center">
+                <div class="titre_inscription">FORMULAIRE D'INSCRIPTION</div>
+                <div class="sous-titre_inscription ">(TPE / PME / ASSOCIATION / STRUCTURE publique ou privée)</div>
+            </div>
+        </div>
+        <!--------------------------partie gauche-1------------------------------>
+        <!------------------------------------------------------------------->
+        <Div class="col-12">
+            <div class="col-5 d-flex align-items-center">
+                <img class="col-3 p-2" src="image/logo blanc.png" alt="">
+                <div class="col-9 d-flex justify-content-end pe-4">
+                    <h1 class="m-0">Dis-Don</h1>
+                </div>
+            </div>
+            <div class="col-12 d-flex">
+                <p class="col-5 sous-titre pe-4">le dispositif Dis-Don est un portail réservé<br>aux organisments et tous utilisateurs<br>domiciliés en France</p>
+
+                <!--------------------------partie droite-1------------------------------>
+                <!------------------------------------------------------------------->
+
+                <div class="col-7 d-flex row justify-content-end text-white">
+                    <div class="col-6 m-0">
+                        <h5>Nom de votre organisme *</h5>
+                        <h6>(Ecrivez ici le nom déclaré en prefecture de votre association, fondation ou fonds de dotation tel qu'il est déclaré en préfecture, et non un diminutif ou un nom d'usage.)</h6>
+                    </div>
+                    
+                    <div class=" col-6 form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 60px"></textarea>
+                        <label for="floatingTextarea2">Comments</label>
+                    </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
