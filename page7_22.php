09e8a2e2cd898e3059c513a7f7fb955630cb9b50
Adrienmaximus <adriencenik@gmail.com>
2024-01-12 22:54:34 +0100

    page7

--- page7_22.php
+++ page7_22.php
@@ -27,15 +27,41 @@
         <!------------------------------------------------------------------------->
 
         <Div class="col-12 p-1 d-flex">
-            <!--------------------------partie droite-1------------------------------>
+            <!--------------------------partie gauche-1------------------------------>
             <!----------------------------------------------------------------------->
             <div class="col-4 d-flex flex-wrap">
                 <div class="col-12 d-flex flex-wrap align-items-center">
                     <img class="col-3 p-2" src="image/logo blanc.png" alt="">
                     <div class="col-8 d-flex justify-content-start text-white pe-3" style="font-size:3rem">Dis-Don</div>
                 </div>
-                <div class="col-10 sous-titre">le dispositif Dis-Don est un portail réservé<br>aux organisments et tous utilisateurs<br>domiciliés en France</div>
+                <div class="col-10 sous-titre">le dispositif Dis-Don est un portail réservé<br>aux organisments et tous utilisateurs<br>domiciliés en France
+                </div>
+                <div class="col-4 text-white pt-2 text-center"> Votre adresse<br>postale *
+                </div>
+                <div class=" col-8 form-floating d-flex flex-wrap pt-1 px-1">
+                    <div class="col-12">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="adresse-postale" style="height: 60px">
+                    </textarea>
+                    </div>
+                    <div class="col-4">
+                        <input type="text" class="form-control text-center" style="height: 30px" id="CP" placeholder="CP" pattern="[0-9]{5}" required>
+                    </div>
+                    <div class="col-8 ps-1">
+                        <input type="text" class="form-control text-center" style="height: 30px" id="" placeholder="Ville">
+                    </div>
+                </div>
+                <div class="col-4 text-white d-flex align-items-center justify-content-end px-2 pb-4"> téléphone *
+                </div>
+                <div class="col-8 d-flex flex-wrap justify-content-between px-1">
+                    <div class="col-3">
+                        <input type="text" class="form-control" id="" aria-describedby="" placeholder="+33" pattern="[0-9]{3}" required>
+                    </div>
+                    <div class="col-9 ps-1">
+                        <input type="text" class="form-control" id="" aria-describedby="" pattern="[0-9]{10}" required>
+                    </div>
+                </div>
             </div>
+
 
             <!--------------------------partie droite-1------------------------------>
             <!----------------------------------------------------------------------->
@@ -53,20 +79,51 @@
                     <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 60px"></textarea>
                     <label class="text-dark opacity-25 text-center" for="floatingTextarea2">100 caractères</label>
                 </div>
-                <div class="col-6 text-white d-flex align-items-center justify-content-end px-4"> Numéro de SIRET</div>
-                <div class="col-6">
-                        <div class="form-floating^">
-                            <input type="text" class="form-control text-center" id="siretNumber" placeholder="14 chiffres" pattern="[0-9]{14}" required>
-
-                        </div>
+                <div class="col-6 text-white d-flex align-items-center justify-content-end px-4"> Numéro de SIRET
                 </div>
-                <div class="col-6 text-white d-flex justify-content-end px-4 pt-3"> Votre champs d'activité *</div>
+                <div class="col-6">
+                    <div class="form-floating^">
+                        <input type="text" class="form-control text-center" id="siretNumber" placeholder="14 chiffres" pattern="[0-9]{14}" required>
+                    </div>
+                </div>
+                <div class="col-6 text-white d-flex justify-content-end px-4 pt-3"> Votre champs d'activité *
+                </div>
                 <div class=" col-6 form-floating py-2">
                     <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 60px"></textarea>
                     <label class="text-dark opacity-25" for="floatingTextarea2">100 caractères</label>
                 </div>
+                <div class="col-6 text-white d-flex align-items-center justify-content-end px-4"> Votre email *
+                </div>
+                <div class="col-6">
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                </div>
+                <div class="col-6 d-flex flex-wrap">
+                    <div class="col-12 text-white d-flex align-items-center justify-content-end px-4"> Mot de passe *
+                    </div>
+                    <div class="col-12 sous-titre-2 px-4 ps-5 text-end">
 
+                        (8 car. min dont 1 majuscule, 1 chiffre et 1 miniscule )
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                </div>
             </div>
+        </div>
+        <!-------------------------PARTIE Inférieur---------------------------->
+        <!--------------------------------------------------------------------->
+        <div class="col-12 d-flex justify-content-center">
+        <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  <label class="form-check-label text-white" for="flexCheckDefault">
+  En cochant cette case, mon organisme accepte les Conditions Générales d'Utilisation.
+  </label>
+</div>
+</div>
         </div>
     </div>
     </div>
