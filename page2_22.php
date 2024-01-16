f7a9eaf9832c68fec208301080ca1bd867a9d9f6
Adrienmaximus <adriencenik@gmail.com>
2024-01-16 16:16:51 +0100

    page2

--- page2_22.php
+++ page2_22.php
@@ -4,30 +4,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gradient Example</title>
+    <title>Page 2-Lancement Prochain</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
     <body>
         <div class="container-fluid" style="max-height: 560px">
-            <!-----------------------------Partie sup------------------------------->
-            <div class="col-12 mt-5 p-3 d-flex justify-content-between">
-                <div class="col-5 text-center flex-wrap text-white m-3 p-3">
+            <!------------------------------------------------------------>
+            <div class="col-12 p-2 d-flex flex-wrap justify-content-between">
+                <!------------------------------A-GAUCHE---------------------->
+                <div class="col-7 mt-3 flex-wrap p-3">
+                    <div class="col-12 ">
+                        <h1 class="oxygen">Lancement prochain<h1>
+                        <div class="col-7 sous-titre-4">Promouvoir la solidarité, réduire le gaspillage et encourager le
+                            partage en facilitant la redistribution des biens excédentaires
+                            ou sous-utilisés, c’est l’objectif de :</div>
+                    </div>
                     <!-- Votre contenu ici -->
-                    <p>> Sur cette page, on peut faire du mécénat ou la
-                        promotion d’ associations qui le souhaitent en
-                        déposant leur bannière.</p>
-                    <!-------------------------- BANNIERE ----------------------------------->
+                    <div class="col-9 d-flex align-items-center justify-content-end">
+                        <img class="col-3 p-2" style="width:15%;" src="image/Image_004.png" alt="logo noir">
+
+                        <div class="col-9 d-flex align-items-center text-dark" style="font-size:70px;font-weight:bold;font-family: Arial, Helvetica, sans-serif;">
+                            Dis-DON
+                        </div>
+                    </div>
+                    <div class="col-9 sous-titre-4">
+                        <p>Un dispositif pratique pour centraliser les dons sous toutes ses formes et connecter
+                            les personnes souhaitant donner avec celles qui ont besoin de ces ressources.
+                            En évitant le gaspillage et en encourageant le recyclage, une telle plateforme
+                            contribue à préserver l'environnement tout en favorisant un esprit de communauté
+                            et de soutien mutuel.</p>
+                    </div>
                 </div>
-                <div class="flex-wrap col-6 d-flex justify-content-end" >
-                    <img src="image/Image_002_3.png" style="width:75%;" alt="image version portable">
+                <!------------------------- A DROITE ----------------------------------->
+                <div class="flex-wrap col-5 d-flex justify-content-center">
+                    <img class="d-flex pb-5 mt-4 pt-1 justify-content-center" src="image/image_002-4.png" style="width:60%; height:100%" alt="image version portable">
                 </div>
             </div>
         </div>
